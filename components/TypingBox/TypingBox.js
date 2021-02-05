562459414ab2f7426feb6b9440a208fb70c1385a
Donte Truong <dontetruong@gmail.com>
2021-02-04 19:30:00 -0600

    TypingBox styles

--- components/TypingBox/TypingBox.js
+++ components/TypingBox/TypingBox.js
@@ -1,12 +1,12 @@
 import Passage from './Passage/Passage.js'
 import TypingInput from './TypingInput/TypingInput.js'
 
-import './TypingBox.module.css'
+import TypingBoxStyles from './TypingBox.module.css'
 
 export default function TypingBox() {
     return (
-        <main>
-            <h1>Code Typer</h1>
+        <main className={TypingBoxStyles.main}>
+            <h1 className={TypingBoxStyles.h1}>Code Typer</h1>
             <Passage />
             <TypingInput />
         </main>
