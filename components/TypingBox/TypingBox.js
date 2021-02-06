acc0d964845cacef2bb06ebaf8f8a0edf5da41dc
Donte Truong <dontetruong@gmail.com>
2021-02-05 22:06:55 -0600

    Sentencer package installation

--- components/TypingBox/TypingBox.js
+++ components/TypingBox/TypingBox.js
@@ -1,14 +1,27 @@
+import {useState} from 'react'
+
 import Passage from './Passage/Passage.js'
 import TypingInput from './TypingInput/TypingInput.js'
 
 import TypingBoxStyles from './TypingBox.module.css'
 
 export default function TypingBox() {
+    const [text, setText] = useState('')
+    const [passageText, setPassageText] = useState([])
+
+    const handleChange = ({target}) => {
+        setText(target.value)
+    }
+
+    const generateNewPassage = () => {
+        
+    }
+
     return (
         <main className={TypingBoxStyles.main}>
             <h1 className={TypingBoxStyles.h1}>Code Typer</h1>
-            <Passage />
-            <TypingInput />
+            <Passage text={passageText}/>
+            <TypingInput value={text} onChange={handleChange}/>
         </main>
     )
 }
--- components/TypingBox/TypingInput/TypingInput.js
+++ components/TypingBox/TypingInput/TypingInput.js
@@ -1,10 +1,18 @@
+import TypingInputStyles from './TypingInput.module.css'
 
-
-export default function TypingInput() {
+export default function TypingInput(props) {
     return (
-        <form>
-            <input type="text"/>
-            <button>Redo</button>
-        </form>
+        <div className={TypingInputStyles.div}>
+            <form className={TypingInputStyles.form}>
+                <input 
+                    className={TypingInputStyles.input}
+                    type="text"
+                    placeholder="Type the words here"
+                    onChange={props.onChange}
+                    value={props.value}
+                />
+            </form>
+            <button className={TypingInputStyles.button}>Redo</button>
+        </div>
     )
 }
