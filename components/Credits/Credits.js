1b7063c0ead64de9d38e3dd68a59c383c205b89d
Donte Truong <dontetruong@gmail.com>
2021-02-09 19:08:13 -0600

    Footer changes

--- components/Credits/Credits.js
+++ components/Credits/Credits.js
@@ -3,7 +3,7 @@ import CreditStyles from './Credits.module.css'
 export default function Credits() {
     return (
         <footer className={CreditStyles.footer}>
-            <p>Made by Donte Truong</p>
+            <p>Made by Sedge</p>
             <p>Code: <a className={CreditStyles.a} href="https://github.com/PandaStripes11/code-typer">https://github.com/PandaStripes11/code-typer</a></p>
         </footer>
     )
