fd15cd85804903a41006929188a158759e0ea61c
Donte Truong <dontetruong@gmail.com>
2021-02-26 17:38:11 -0600

    More bonuses addition

--- utils/wpmBonusesData.js
+++ utils/wpmBonusesData.js
@@ -5,7 +5,9 @@ const wpmBonusesData = [
     {cost: 1000, bonus: 30},
     {cost: 1500, bonus: 40},
     {cost: 2000, bonus: 50},
-    {cost: 3000, bonus: 60}
+    {cost: 3000, bonus: 60},
+    {cost: 5000, bonus: 80},
+    {cost: 7000, bonus: 100}
 ]
 
 export {wpmBonusesData}
