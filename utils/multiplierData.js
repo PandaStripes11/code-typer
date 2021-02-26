340cd05608b0713e3109fb15e5da131af242a77b
Donte Truong <dontetruong@gmail.com>
2021-02-26 16:06:53 -0600

    Multiplier changes

--- utils/multiplierData.js
+++ utils/multiplierData.js
@@ -9,9 +9,10 @@ const MultiplierData = [
     {cost: 8000, multiplier: 14},
     {cost: 12000, multiplier: 20},
     {cost: 16000, multiplier: 30},
+    {cost: 20000, multiplier: 40},
     {cost: 30000, multiplier: 50},
-    {cost: 50000, multiplier: 75},
-    {cost: 75000, multiplier: 100}
+    {cost: 50000, multiplier: 70},
+    {cost: 80000, multiplier: 100}
 ]
 
 export {MultiplierData}
