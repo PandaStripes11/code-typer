75604d270924f0506c6f83bd54719af63be50d8e
Donte Truong <dontetruong@gmail.com>
2021-02-27 15:58:50 -0600

    Passive income levels addition

--- utils/passiveIncomeData.js
+++ utils/passiveIncomeData.js
@@ -8,7 +8,11 @@ const passiveIncomeData = [
     {cost: 10000, income: 30},
     {cost: 20000, income: 40},
     {cost: 40000, income: 60},
-    {cost: 60000, income: 80}
+    {cost: 60000, income: 80},
+    {cost: 80000, income: 100},
+    {cost: 100000, income: 120},
+    {cost: 140000, income: 160},
+    {cost: 180000, income: 200}
 ]
 
 export {passiveIncomeData}
