17d2349328b6984a7d2b16118fa1ac603035edd7
Donte Truong <dontetruong@gmail.com>
2021-02-27 16:02:09 -0600

    Wpm bonus levels addition

--- utils/wpmBonusesData.js
+++ utils/wpmBonusesData.js
@@ -7,7 +7,13 @@ const wpmBonusesData = [
     {cost: 2000, bonus: 50},
     {cost: 3000, bonus: 60},
     {cost: 5000, bonus: 80},
-    {cost: 7000, bonus: 100}
+    {cost: 7000, bonus: 100},
+    {cost: 10000, bonus: 120},
+    {cost: 15000, bonus: 150},
+    {cost: 20000, bonus: 200},
+    {cost: 40000, bonus: 300},
+    {cost: 60000, bonus: 350},
+    {cost: 100000000, bonus: 100000000}
 ]
 
 export {wpmBonusesData}
